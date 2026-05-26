6c2c101991ee8824efd9dccca5904350ac177e1a
lunar <dave.w.whitley@gmail.com>
2026-05-26 15:20:35 -0500

    Add files via upload

---
+++ thisisafile.py
@@ -0,0 +1,25 @@
+import math
+import turtle
+import tkinter as tk
+import datetime
+import time
+
+print("Haaaaaaaaaaaaaallo")
+
+t = turtle.Turtle()
+for _ in range(4):
+    t.forward(100)
+    t.left(90)
+    t.right(4)
+
+while True:
+
+    root = tk.Tk()
+
+root.title("Hehe")
+
+root.geometry("400x300")
+
+root.mainloop()
+
+time.sleep(5)
