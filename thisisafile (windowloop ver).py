9bdf4d4a9b8448f361674bb282f70548587dec37
lunar <dave.w.whitley@gmail.com>
2026-05-26 15:22:48 -0500

    Add files via upload

---
+++ thisisafile (windowloop ver).py
@@ -0,0 +1,19 @@
+import math
+import turtle
+import tkinter as tk
+import datetime
+import time
+
+print("Haaaaaaaaaaaaaallo")
+
+while True:
+
+    root = tk.Tk()
+
+    root.title("HEHEHEHE")
+
+    root.geometry("400x300")
+
+    root.mainloop()
+
+    time.sleep(5)
